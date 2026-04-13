ed01f41aac99d354a743169c5d455d8fe238529e
drcharleskamen-png <dr.charles.kamen@gmail.com>
2026-04-12 17:36:15 -0700

    Add Instagram handle text to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -48,6 +48,7 @@ export default function Footer() {
               <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
               <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
             </svg>
+            <span>@livenow_longevity</span>
           </a>
         </div>
 
